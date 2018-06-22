6e2466d5bc5db21ede73471e33f7129051689184
No Name <den4ikept11@gmail.com>
2018-06-22 13:36:28 +0500

    fix

--- redirector.py
+++ redirector.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
 import json
-import syslog
 import logging
 from datetime import datetime
 from os.path import getmtime
@@ -21,20 +20,16 @@ def main():
 		if (time_change != int(getmtime(FILENAME))):
 			redirect_urls = json.load(open(FILENAME))
 			time_change = int(getmtime(FILENAME))
-			logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ": FILE CHANGE" )
-		else:
-			logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ": FILE not CHANGE {}".format(time_change))
-		logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + 'request: ' + request )
-
+		logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' request: ' + request )
 		request = request.split(' ')
-		logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + 'url: ' + request[0] +'\n')
+		logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' url: ' + request[0] +'\n')
 		response = '' 
 		for url in redirect_urls.keys():
 			if url in request[0]:
 				response = 'OK rewrite-url="{}"'.format(redirect_urls[url])
 				break 
 		if response:
-			logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + 'CHANGE: ' + response +'\n')
+			logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' redirect: ' + response +'\n')
 		sys.stdout.write(response + "\n")
 		sys.stdout.flush()
 		request = sys.stdin.readline()
